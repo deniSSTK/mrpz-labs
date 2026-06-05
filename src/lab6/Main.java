c7cdd0f4182d3b4dc87270fad1ffa2674274b08b
deniSSTK <denisdrawler@gmail.com>
2026-06-05 08:51:09 +0300

    implement lab6

--- src/lab6/Main.java
+++ src/lab6/Main.java
@@ -1,37 +1,36 @@
+package lab6;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.print("Enter the size of the list: ");
         int size = scanner.nextInt();
-        
+
         ArrayList<Integer> list = new ArrayList<>();
         System.out.println("Enter " + size + " integers:");
         for (int i = 0; i < size; i++) {
             list.add(scanner.nextInt());
         }
-        
+
         System.out.println("Original list: " + list);
-        
+
         System.out.print("Enter the number of positions to shift right (N): ");
         int n = scanner.nextInt();
-        
-        if (n < 0 || n >= size) {
-            System.out.println("Invalid N. N must be between 0 and " + (size - 1));
+
+        if (n < 0) {
+            System.out.println("Invalid N. N must be non-negative.");
             return;
         }
-        
-        for (int i = 0; i < n; i++) {
-            Integer last = list.remove(list.size() - 1);
-            list.add(0, last);
-        }
-        
+
+        Collections.rotate(list, n);
+
         System.out.println("Modified list after cyclic right shift by " + n + " positions: " + list);
-        
+
         scanner.close();
     }
 }
